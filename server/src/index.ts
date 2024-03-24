8a91cc629069aed911472ed928fd63515bffb1fd
santiMintegui <santiagomintegui@gmail.com>
2024-03-24 16:10:16 -0300

    fix cors in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,10 @@ import { PORT } from "./constants";
 import router from "./routes";
 import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware";
 import authMiddleware from "./middlewares/authMiddleware";
+import cors from "cors";
 
 const app: Express = express();
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at http://localhost:${PORT}`);
